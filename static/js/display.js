00296295ffd90df2e17d8bc4e47e950d14c9bdad
Jared <jkrogsrud@gmail.com>
2024-08-02 11:27:44 -0400

    Issues with animation so far, going to branch a copy that just displays score and then do some weird stuff on the other branch

--- static/js/display.js
+++ static/js/display.js
@@ -29,7 +29,7 @@ socket.on('reset', function() {
     // The only data held by display is in the textContent of current_score class
     let scores = document.getElementsByClassName('current_score');
 
-    scores.forEach((score) => {
+    Array.from(scores).forEach((score) => {
         score.textContent = 0;
     });
 });
@@ -41,15 +41,55 @@ class Player_Animate {
         this.end_score = end_score;
         this.start_pos = start_pos;
         this.end_pos = end_pos;
-    }
+
+        this.total_movement = 20 * (this.end_pos - this.start_pos);
+
+    };
+
+    draw(progress) {
+        // progress is from 0 to 1
+        console.log(this.name);
+        console.log(this.total_movement);
+        return this.total_movement * progress + 'rem';
+    };
 };
 
 class Score_Animate {
-    constructor(players, time_interval) {
+    constructor(players) {
         this.players = players;
-        this.time_interval = time_interval;
-    }
-}
+    };
+
+    draw_info(progress) {
+        let all_movement = [];
+        this.players.forEach((player) => {
+            all_movement.push({'name': player.name, 'draw': player.draw(progress)});
+        });
+        return all_movement;
+    };
+
+};
+
+// Helper function for animation
+function animate({timing, draw, duration}) {
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction goes from 0 to 1
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // calculate the current animation state
+    let progress = timing(timeFraction);
+
+    draw(progress); // draw it
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    };
+
+  });
+};
 
 socket.on('show_score_animation', function(score_bundle) {
 
@@ -118,25 +158,65 @@ socket.on('show_score_animation', function(score_bundle) {
     // start and end _pos are the relative location
     let all_player_animates = [];
 
-    while (let i = 0; i < sorted_players_prev.length; i++) {
+    for (let i = 0; i < sorted_players_prev.length; i++) {
         // sorted_players_prev[i][0] is the name
         const player_name = sorted_players_prev[i][0]
         const start_score = prev_scores[player_name];
         const end_score = new_scores[player_name];
         const start_pos = i;
-
+        var end_pos = -1;
         // Loop through the sorted_players_new to find the new positions
-        while (let j = 0; j < sorted_players_new.length; j++) {
+        for (let j = 0; j < sorted_players_new.length; j++) {
             if (sorted_players_new[j][0] == player_name) {
-                const end_pos = j;
-            }
+                end_pos = j;
+            };
         };
 
-        const player_animate = Player_Animate(player_name, start_score, end_score, start_pos, end_pos);
+        const player_animate = new Player_Animate(player_name, start_score, end_score, start_pos, end_pos);
         all_player_animates.push(player_animate);
     };
 
 
+    const score_animate = new Score_Animate(all_player_animates);
 
+//    // now we animate
+//    animate({
+//        duration: 10000,
+//        timing: function(timeFraction) {
+//            return timeFraction;
+//        },
+//        draw: function(progress) {
+//            let draw_info = score_animate.draw_info(progress);
+//
+//            draw_info.forEach((player_info) => {
+//                let player_id = player_info['name'];
+//                let player_movement = player_info['draw'];
+//
+//                let player_div = document.getElementById(player_id);
+//
+//                player_div.style.left = player_movement;
+//
+//            });
+//        }
+//    });
+    // test animation
+
+    for (let i= 0; i < 10; i++){
+        console.log(typeof(i));
+        let progress = i / 10;
+        console.log(progress);
+        next_frame = score_animate.draw_info(progress);
+        console.log(next_frame);
+        // This should be a number of pairs {name, draw_distance}
+        next_frame.forEach((player_info) => {
+            player_id = player_info['name'];
+            player_movement = player_info['draw'];
+
+            const player_div = document.getElementById(player_id);
+
+            player_div.style.left = player_movement;
+
+        })
+    };
 
 });
